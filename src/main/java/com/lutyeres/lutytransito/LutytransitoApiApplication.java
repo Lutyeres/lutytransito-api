7a603dd6f95b9e28d5364fcda90d6f6eed63a9bf
Savyo Lutyeres <lutyeres@gmail.com>
2026-01-15 15:06:56 -0300

    First Commit - Create API

--- src/main/java/com/lutyeres/lutytransito/LutytransitoApiApplication.java
+++ src/main/java/com/lutyeres/lutytransito/LutytransitoApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LutytransitoApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(LutytransitoApiApplication.class, args);
     }
 
---
+++ src/main/java/com/lutyeres/lutytransito/api/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.lutyeres.lutytransito.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+
+    @GetMapping("/teste")
+    public String testar(){
+        return "testando 123";
+    }
+
+}
