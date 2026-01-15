2fed58a96cd7f491b253268f9c729e29934d3ae4
Savyo Lutyeres <lutyeres@gmail.com>
2026-01-15 20:27:16 -0300

    Mapeamento da Entidade

--- src/main/java/com/lutyeres/lutytransito/domain/model/Proprietario.java
+++ src/main/java/com/lutyeres/lutytransito/domain/model/Proprietario.java
@@ -1,15 +1,29 @@
 package com.lutyeres.lutytransito.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity //Mapeamento do Objeto Relacional com o Banco de dados
 public class Proprietario {
 
+    @EqualsAndHashCode.Include //Anotação para incluir o ID no Equals and HashCode
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "propId")
     private Long id;
+
+    @Column(name = "propNome")
     private String nome;
+
+    @Column(name = "propEmail")
     private String email;
+
+    @Column(name = "propTelefone")
     private String telefone;
 
 
--- src/main/resources/db/migration/V001__create-table-proprietario.sql
+++ src/main/resources/db/migration/V001__create-table-proprietario.sql
@@ -1,8 +1,8 @@
 CREATE TABLE proprietario (
-                              prop_id BIGINT NOT NULL AUTO_INCREMENT,
-                              prop_nome VARCHAR(60) NOT NULL,
-                              prop_email VARCHAR(225) NOT NULL,
-                              prop_telefone VARCHAR(20) NOT NULL,
-                              PRIMARY KEY (prop_id),
-                              UNIQUE INDEX prop_email_UNIQUE (prop_email ASC)
+                              propId BIGINT NOT NULL AUTO_INCREMENT,
+                              propNome VARCHAR(60) NOT NULL,
+                              propEmail VARCHAR(225) NOT NULL,
+                              propTelefone VARCHAR(20) NOT NULL,
+                              PRIMARY KEY (propId),
+                              UNIQUE INDEX propEmail_UNIQUE (propEmail ASC)
 );
