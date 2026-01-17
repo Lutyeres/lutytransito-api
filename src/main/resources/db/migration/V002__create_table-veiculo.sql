9ce6523ca9152fd51851084cd9b3f5e82a66b254
Savyo Lutyeres <lutyeres@gmail.com>
2026-01-16 21:20:11 -0300

    Create V002__create_table-veiculo.sql

---
+++ src/main/resources/db/migration/V002__create_table-veiculo.sql
@@ -0,0 +1,17 @@
+CREATE TABLE veiculo` (
+                                          `veicId` BIGINT NOT NULL AUTO_INCREMENT,
+                                          `propId` BIGINT NOT NULL,
+                                          `veicMarca` VARCHAR(20) NOT NULL,
+                                          `veicModelo` VARCHAR(20) NOT NULL,
+                                          `veicPlaca` VARCHAR(7) NOT NULL,
+                                          `veicDataApreensao` DATETIME NULL,
+                                          `veicStatus` VARCHAR(20) NOT NULL,
+                                          `veicDataCadastro` DATETIME NOT NULL,
+                                          PRIMARY KEY (`veicId`),
+                                          UNIQUE INDEX `veicPlaca_UNIQUE` (`veicPlaca` ASC) VISIBLE,
+                                          INDEX `fk_veiculo_proprietario_idx` (`propId` ASC) VISIBLE,
+                                          CONSTRAINT `fk_veiculo_proprietario`
+                                              FOREIGN KEY (`propId`)
+                                                  REFERENCES `lutytransito`.`proprietario` (`propId`)
+                                                  ON DELETE NO ACTION
+                                                  ON UPDATE NO ACTION);
